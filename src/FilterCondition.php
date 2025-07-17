3bea52d6ea7d4ce64ee1627115407953a071e69c
Plokko <mail@fpellerone.it>
2025-07-17 10:40:22 +0200

    fix

--- src/FilterCondition.php
+++ src/FilterCondition.php
@@ -246,7 +246,7 @@ class FilterCondition
     protected static function _isEmpty($v)
     {
         //return empty($v);//$v === null || trim("$v") == '';
-        return return $v === null || (is_string($v) && trim("$v") == '') || empty($v);
+        return $v === null || (is_string($v) && trim("$v") == '') || empty($v);
     }
 }
 
